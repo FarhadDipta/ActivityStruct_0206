62045eb5424ea263029ed272715ff405a0605063
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-02 14:50:52 +0700

    menyelasaikan struktur pertama

--- Struct.cpp
+++ Struct.cpp
@@ -10,4 +10,16 @@ struct Mahasiswa{
 
 int main(){
     Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : " << endl;
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "\nNim : " << mhs.nim;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nAlamat : " << mhs.alamat;
+    cout << "\nUmur : " << mhs.umur;
 }
