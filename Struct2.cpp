30cb2edb1610522007ea410e1905449608616032
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-02 14:52:46 +0700

    menambahkan struktur detail alamat

---
+++ Struct2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat{
+    
+};
+
+struct Mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main(){
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : " << endl;
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "\nNim : " << mhs.nim;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nAlamat : " << mhs.alamat;
+    cout << "\nUmur : " << mhs.umur;
+}
