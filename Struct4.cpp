46af465993ac0eec7d727d8696c542056159a295
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-02 15:20:10 +0700

    memperbaiki dan selesai

--- Struct4.cpp
+++ Struct4.cpp
@@ -37,7 +37,7 @@ int main(){
 
     for (int i = 0; i < 3; i++){
     cout << endl;
-    cout << "Data Mahasiswa ke-" << (i+1) << end;
+    cout << "Data Mahasiswa ke-" << (i+1) << endl;
     cout << "\nNim : " << mhs[i].nim;
     cout << "\nNama : " << mhs[i].nama;
     cout << "\nAlamat : ";
