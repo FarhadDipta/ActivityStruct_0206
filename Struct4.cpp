f618bc511ca6eaedfe56dba3387c6dcd0a074184
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-02 15:19:49 +0700

    menambahkan variasi kedua

--- Struct4.cpp
+++ Struct4.cpp
@@ -37,6 +37,7 @@ int main(){
 
     for (int i = 0; i < 3; i++){
     cout << endl;
+    cout << "Data Mahasiswa ke-" << (i+1) << end;
     cout << "\nNim : " << mhs[i].nim;
     cout << "\nNama : " << mhs[i].nama;
     cout << "\nAlamat : ";
